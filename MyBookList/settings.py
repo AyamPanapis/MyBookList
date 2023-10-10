cc62619d956fbe2da8493c943099ef54533bb0bf
Hezekial Octora Yudha Tampubolon <hezekial7778@gmail.com>
2023-10-10 13:01:26 +0700

    Setting up project

--- MyBookList/settings.py
+++ MyBookList/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Taiwlind Initialaization
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH=r"C:\Program Files\nodejs\npm.cmd"
