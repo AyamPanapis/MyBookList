af108fe2f3a94fd9eeb8a9ee0faeda46126368d7
Hezekial Octora Yudha Tampubolon <hezekial7778@gmail.com>
2023-10-25 18:58:07 +0700

    Integrate Google Books API

--- main/models.py
+++ main/models.py
@@ -2,3 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)  # ISBN number as a unique identifier
+    cover_url = models.URLField()
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.title
