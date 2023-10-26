1041322e4711990623c9813660df56873209878a
ardhika23 <ardhika@gmail.com>
2023-10-26 18:07:44 +0700

    Clean

--- book/urls.py
+++ book/urls.py
@@ -5,4 +5,7 @@ app_name = 'book'
 
 urlpatterns = [
     path('<str:id>/', show_books, name='show_books'),
+    path('review/add/<str:book_id>/', add_review, name='add_review'),  # assuming book_id is a string, change to int if it's an integer
+    path('review/user/<str:username>/', user_review_list, name='user_review_list'),
+    path('review/user/', user_review_list, name='user_review_list_self'),
 ]
