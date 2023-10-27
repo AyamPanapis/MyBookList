3d999c6a52ccbeab84fb15897210f9b9eb23e4ce
Hezekial Octora Yudha Tampubolon <hezekial7778@gmail.com>
2023-10-27 16:53:52 +0700

    Cleaning repo

--- book/models.py
+++ book/models.py
@@ -5,7 +5,6 @@ from dataset.models import Book
 class Review(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user_name = models.CharField(max_length=200)
-    rating = models.IntegerField()
     comment = models.TextField()
     pub_date = models.DateTimeField('date published')
     RATING_CHOICES = (
@@ -15,11 +14,6 @@ class Review(models.Model):
         (4, '4'),
         (5, '5'),
     )
-    
-    book = models.ForeignKey(Book, on_delete=models.CASCADE,)  # Changed to CASCADE for integrity
-    pub_date = models.DateTimeField('date published')
-    user_name = models.CharField(max_length=100)
-    comment = models.CharField(max_length=200)
     rating = models.IntegerField(choices=RATING_CHOICES)
 
     def __str__(self):
