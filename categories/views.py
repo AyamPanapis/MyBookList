02587c86eacadda3708f79acb72c5aaca3a2d6f8
rakhafahimshahab <rakha.fahim@gmail.com>
2023-10-28 15:29:43 +0700

    updating categories

--- categories/views.py
+++ categories/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 import requests
 from categories.models import Category
 from dataset.models import Book
+from django.http import HttpResponse
+from django.core import serializers
+from django.http import JsonResponse
 
 def show_category(request):
     distinct_categories = Book.objects.values_list('categories', flat=True).distinct()
@@ -15,3 +18,9 @@ def books_by_category(request, category_name):
     books = Book.objects.filter(categories=category_name)
     template_name = f'{category_name}.html'
     return render(request, template_name, {'books': books, 'category_name': category_name})
+
+
+def get_book_json(request):
+    book = Book.objects.filter(categories='Fiction')
+    data = serializers.serialize('json', book)
+    return HttpResponse(data, content_type='application/json')
