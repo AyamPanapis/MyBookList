f28c99b0995a4601a909687f09e9a03d3232de27
rakhafahimshahab <rakha.fahim@gmail.com>
2023-10-29 14:35:07 +0700

    updating categories

--- categories/urls.py
+++ categories/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from categories.views import books_by_category,show_category,get_book_json
+from categories.views import books_by_category,show_category,get_book_json,get_book_json_all,show_all
 
 app_name = 'categories'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', show_category, name='show-category'),
     path('<str:category_name>/',books_by_category, name='books_by_category'),
     path('<str:category_name>/books_json/', get_book_json, name='get_book_json'),
+    path('all/', show_all, name='show_all'),
+    path('all/books_json_all/', get_book_json_all, name='get_book_json_all'),
 ]
