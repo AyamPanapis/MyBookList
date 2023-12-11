c451d776fc8eb4c10e64f844dae042764e249962
Hezekial Octora Yudha Tampubolon <hezekial7778@gmail.com>
2023-12-11 15:30:51 +0700

    Try Deploy 4

--- dataset/models.py
+++ dataset/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    publisher = models.CharField(max_length=100)
+    title = models.CharField(max_length=1000)
+    author = models.CharField(max_length=1000)
+    publisher = models.CharField(max_length=1000)
     description = models.TextField()
-    categories = models.CharField(max_length=100)
-    image_link = models.CharField(max_length=100)
+    categories = models.CharField(max_length=1000)
+    image_link = models.CharField(max_length=1000)
