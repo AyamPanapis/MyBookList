9cd3bb08e1fc633ae2257643f2fcfc735e3c1ae3
Hezekial Octora Yudha Tampubolon <hezekial7778@gmail.com>
2023-12-11 14:38:17 +0700

    Try Deploy

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from authentication.views import register, login_user, logout_user, login_flutter, register_flutter, logout_flutter
+from authentication.views import register, login_user, logout_user, login_flutter, register_flutter, logout_flutter, show_json_by_id
+
 
 
 app_name = 'authentication'
@@ -11,4 +12,5 @@ urlpatterns = [
     path('login_flutter/', login_flutter, name='login_flutter'),
     path('register_flutter/', register_flutter, name='register_flutter'),
     path('logout_flutter/', logout_flutter, name='logout_flutter'),
+    path('json/', show_json_by_id, name='show_json_by_id'),
 ]
