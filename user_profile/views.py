17bbf5b64e6488dd4575551967c3a20aa6f50a79
Revaldyhfz <revaldyhafiz16@gmail.com>
2023-12-15 21:44:24 +0700

    Booklist backend flutter

--- user_profile/views.py
+++ user_profile/views.py
@@ -8,6 +8,12 @@ from book.models import WishList
 from django.http import JsonResponse
 from django.core import serializers
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
 
 @login_required(login_url='/auth/login/')
 def show_profile(request, id):
@@ -45,7 +51,7 @@ def get_planned_json(request):
     planned_to_read_book_ids = WishList.objects.filter(user=user, book_list=0).values_list('book', flat=True)
 
     planned_to_read_books = Book.objects.filter(id__in=planned_to_read_book_ids)
-    return HttpResponse(serializers.serialize('json', planned_to_read_books))
+    return HttpResponse(serializers.serialize('json', planned_to_read_books), content_type="application/json")
 
 def get_reading_json(request):
     user = request.user
@@ -53,14 +59,40 @@ def get_reading_json(request):
     reading_book_ids = WishList.objects.filter(user=user, book_list=1).values_list('book', flat=True)
 
     reading_books = Book.objects.filter(id__in=reading_book_ids)
-    return HttpResponse(serializers.serialize('json', reading_books))
+    return HttpResponse(serializers.serialize('json', reading_books), content_type="application/json")
+
 
 def get_completed_json(request):
     user = request.user
 
-    completed_book_ids = WishList.objects.filter(user=user.username, book_list=2).values_list('book', flat=True)
+    completed_book_ids = WishList.objects.filter(user=user, book_list=2).values_list('book', flat=True)
 
     completed_books = Book.objects.filter(id__in=completed_book_ids)
-    return HttpResponse(serializers.serialize('json', completed_books))
+    return HttpResponse(serializers.serialize('json', completed_books),content_type="application/json")
+
+@csrf_exempt
+def get_planned_json_flutter(request):
+    user = request.user
+    planned_to_read_book_ids = WishList.objects.filter(user=user.pk,book_list=0).values_list('book', flat=True)
+
+    planned_to_read_books = Book.objects.filter(id__in=planned_to_read_book_ids)
+    return HttpResponse(serializers.serialize('json', planned_to_read_books), content_type="application/json")
+
+@csrf_exempt
+def get_reading_json_flutter(request):
+    user = request.user
+    reading_book_ids = WishList.objects.filter(user=user.pk,book_list=1).values_list('book', flat=True)
+
+    reading_books = Book.objects.filter(id__in=reading_book_ids)
+    return HttpResponse(serializers.serialize('json', reading_books), content_type="application/json")
+
+@csrf_exempt
+def get_completed_json_flutter(request):
+    user = request.user
+    completed_book_ids = WishList.objects.filter(user=user.pk,book_list=2).values_list('book', flat=True)
+
+    completed_books = Book.objects.filter(id__in=completed_book_ids)
+    return HttpResponse(serializers.serialize('json', completed_books),content_type="application/json")
+
 
 
