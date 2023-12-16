8bfa9dc401ade4c54e40404b88b9d7d4e26c35ee
Hezekial Octora Yudha Tampubolon <hezekial7778@gmail.com>
2023-12-16 22:23:53 +0700

    try deploy 3

--- book/urls.py
+++ book/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from book.views import show_books, to_read, reading, finished_reading, create_review_flutter
+from book.views import show_books, to_read, reading, finished_reading, create_review_flutter, wishlist_flutter, show_review_json
 
 app_name = 'book'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('reading/<int:book_id>/<int:user_id>/', reading, name='reading'),
     path('finished-reading/<int:book_id>/<int:user_id>/', finished_reading, name='finished_reading'),
     path('create-review/', create_review_flutter, name='create_review_flutter'),
+    path('wishlist/<int:book_id>/', wishlist_flutter, name='wishlist_flutter'),
+    path('show-review-json/<int:book_id>/', show_review_json, name='show_review_json'),
 ]
